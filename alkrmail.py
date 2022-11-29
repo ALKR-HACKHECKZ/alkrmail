6e94c9b86ee6f61a61a242d73a0fa18ef66c2097
JASIR JABBAR <105303868+LUCIFIERXT@users.noreply.github.com>
2022-11-29 15:03:32 +0500

    Update alkrmail.py

--- alkrmail.py
+++ alkrmail.py
@@ -5,12 +5,6 @@ import smtplib
 from os import system
 
 def main():
-   print """
-  ____                 _ _ _   _            _
- / __|_ __ ___   __ _(_) | | | | __ _  ___| | __
-| ( | |  _   _ \ / _` | | | |_| |/ _` |/ __| |/ /
-| (_| | | | | | | (_| | | |  _  | (_| | (__|   <
- \____|_| |_| |_|\__,_|_|_|_| |_|\__,_|\___|_|\_\ """
    print '================================================='
    print '               coded by ALKR                     '
    print '     Subscribe My Channel ALKR-HACKHECKZ         '
